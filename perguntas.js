26f5717d6979f276de2c3e548de060661f690a58
julialuzia <161466814+julialuzia@users.noreply.github.com>
2024-11-25 21:17:12 -0300

    Add files via upload
    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Super-herói',
+    'Qual o nome do Homem-Aranha?',
+    'O nome do Homem-Aranha é Peter Parker'
+)
+
+criaCartao(
+    'Série',
+    'Quem é a rainha legítima de "A casa do Dragão"?',
+    'A rainha legítima é a rainha Rhaenyra Targaryen Primeira de Seu nome'
+)
+
+criaCartao(
+    'Música',
+    'Quais os nomes dos membros da banda One Direction?',
+    'Louis, Zayn, Liam, Harry e Niall'
+)
+
+criaCartao(
+    'Filme',
+    'O que Tyler Durden do filme Clube da Luta vende?',
+    'Tyler vende sabonetes'
+)
